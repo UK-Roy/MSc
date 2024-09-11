f508640ff3ade6a3c3e9cc4be03f61717024abc4
UK-Roy <utsharoy99@gmail.com>
2024-09-11 13:47:46 +0600

    RL model added

--- code/testABPhase1.py
+++ code/testABPhase1.py
@@ -1,15 +1,31 @@
 import numpy as np
 import safety_gymnasium
+import torch
+from safepo.common.model import ActorVCritic
 from scipy.optimize import minimize
 
 # Load the learned A and B matrices
 A = np.load('A_matrix.npy')  # Load the pre-trained A matrix
 B = np.load('B_matrix.npy')  # Load the pre-trained B matrix
 
-B_pseudo_inv = np.linalg.pinv(B)
+# Create the safety-task environment
+env = safety_gymnasium.make("SafetyCarGoal2-v0", render_mode="human")
 
+# Reset the environment
+obs, info = env.reset(seed=1)
+
+model_path = "model499.pt"
+
+B_pseudo_inv = np.linalg.pinv(B)
+model = ActorVCritic(
+        obs_dim=env.observation_space.shape[0],
+        act_dim=env.action_space.shape[0],
+        hidden_sizes=[64, 64],
+    )
+
+model.actor.load_state_dict(torch.load(model_path))
 # Parameters
-SAFETY_THRESHOLD = 0.8  # Define a threshold for determining if the LiDAR value is too close (unsafe)
+SAFETY_THRESHOLD = 0.85  # Define a threshold for determining if the LiDAR value is too close (unsafe)
 optimizer_steps = 50  # Number of steps for optimizing the action
 
 # Function to predict the next LiDAR readings
@@ -24,15 +40,14 @@ def unsafe(L_data, safety_threshold):
     return np.any(L_data >= safety_threshold)
 
 def safe_action(L_current, L_next_predicted, action):
-    if unsafe(L_data=L_next_predicted, safety_threshold=SAFETY_THRESHOLD):
-        del_lidar_val = del_lidar(L_current=L_current, L_next_predicted=L_next_predicted)
-        # Compute the pseudoinverse of A
+    del_lidar_val = del_lidar(L_current=L_current, L_next_predicted=L_next_predicted)
+    # Compute the pseudoinverse of A
 
-        # Calculate Delta x using the pseudoinverse
-        del_x = np.dot(B_pseudo_inv, del_lidar_val)
-        action -= del_x
-        # Predict the next LiDAR readings based on the current action
-        L_next_predicted = predict_lidar(L_current, action, A, B)
+    # Calculate Delta x using the pseudoinverse
+    del_x = np.dot(B_pseudo_inv, del_lidar_val)
+    action -= del_x
+    # Predict the next LiDAR readings based on the current action
+    # L_next_predicted = predict_lidar(L_current, action, A, B)
     return action
 
 # Create the safety-task environment
@@ -46,8 +61,14 @@ while True:
     L_current = obs[40:72]  # Convert LiDAR readings to NumPy array
 
     # Initialize action with random values (forces on the wheels)
-    action = env.action_space.sample()  # [F_left, F_right]
+    # action = env.action_space.sample()  # [F_left, F_right]
 
+    eval_obs = torch.as_tensor(obs, dtype=torch.float32)
+    with torch.no_grad():
+                act, _, _, _ = model.step(
+                    eval_obs, deterministic=True
+                )
+    action = act.detach().squeeze().cpu().numpy()
     # Predict the next LiDAR readings based on the current action
     L_next_predicted = predict_lidar(L_current, action, A, B)
     
